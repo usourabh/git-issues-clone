3360a14da89ba26c7f7e54df84c50fe28d33b892
Sourabh Upadhyay <usourabh99@gmail.com>
2023-04-02 17:42:14 +0530

    Added eslint with prettier

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className="text-4xl">Hello, world!</div>
