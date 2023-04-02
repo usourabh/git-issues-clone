1ec783a05848cf5760258df5b123ee5f27731882
Sourabh Upadhyay <usourabh99@gmail.com>
2023-04-02 20:29:02 +0530

    Added initial form state for new issues
    Configured local storage to get and set username

--- src/pages/NewIssue.jsx
+++ src/pages/NewIssue.jsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from 'react';
 import { Layout } from '../components/Layout';
 
 export const NewIssue = () => {
+  const [issueForm, setIssueForm] = useState({
+    title: '',
+    description: '',
+    createdBy: '',
+    createdAt: Date.now(),
+  });
+
+  useEffect(() => {
+    let username = localStorage.getItem('username');
+
+    if (!username) {
+      username = 'usourabh';
+      localStorage.setItem('username', username);
+    }
+
+    setIssueForm((prev) => ({
+      ...prev,
+      createdBy: username,
+    }));
+  }, []);
+
   return <Layout>NewIssue</Layout>;
 };
