aa8cc3ef217495170c3c7be3a4adc7929bb7db2c
Sourabh Upadhyay <usourabh99@gmail.com>
2023-04-02 17:29:32 +0530

    Initalized tailwind

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div>Hello, world!</div>
+    <div className="text-4xl">Hello, world!</div>
   </React.StrictMode>
 );
